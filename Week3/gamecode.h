1431f11b70455b1b7c2e7846ad72232f4d2f7641
William Taylor <williamtaylordev@gmail.com>
2022-02-10 14:30:32 +0000

    Reformat and reorder gamecode.h

--- Week3/gamecode.h
+++ Week3/gamecode.h
@@ -22,69 +22,62 @@
 // Manages a game loop, as well as menu and paused states.
 // The class is not very cohesive - it really needs to be refactored into
 // a few separate classes, but this keeps it simple.
-class Game
-{
+class Game {
 private:
+	// MENU     = start menu
+	// PAUSED   = paused
+	// RUNNING  = the main game loop
+	// GAMEOVER = setting this state causes the program to close after tidying up
 	enum GameState{MENU, PAUSED, RUNNING, GAMEOVER};
-	GameState m_currentState;     // Current state of the game 
-                                  // Menu = start menu
-                                  // Paused = paused
-                                  // Running = the main game loop
-                                  // GAMEOVER = setting this state causes the program to close
-                                  //            after tidying up
-	void ChangeState(GameState newState);  // Use to change the state of the game to one of the states above
-	int m_menuOption;             // Tracks the currently selected menu option, during main or pause menu
+	GameState m_currentState;
 
-	GameTimer gt;                 // Tracks the time between time marks (for getting FPS)
+	int m_menuOption; // Tracks the currently selected menu option, during main or pause menu
+	GameTimer gt;     // Tracks the time between time marks (for getting FPS)
 
 	// Disable:
 	Game();            // no-arg constructor
 	Game(Game& other); // copy constructor
 	~Game();           // destructor
 
-
+	void ChangeState(GameState newState);  // Use to change the state of the game to one of the states above
 
 public:
 	static Game instance;          // Singleton instance
 
-   // Starts the game engines - Draw Engine, Sound Engine, Input Engine - singletons
-   // This is called soon after the program runs
+	// Starts the game engines - Draw Engine, Sound Engine, Input Engine - singletons
+	// This is called soon after the program runs
 	ErrorType Setup(bool bFullScreen, HWND hwnd, HINSTANCE hinstance);
 
-   // Terminates the game engines - Draw Engine, Sound Engine, Input Engine
-   // This is called just before the program exits
+	// Terminates the game engines - Draw Engine, Sound Engine, Input Engine
+	// This is called just before the program exits
 	void Shutdown();
 
-   // This is repeated, called every frame.
-   // It will run either Update( ), MainMenu() or PauseMenu() depending on the
-   // game state
+	// This is repeated, called every frame.
+	// It will run either Update( ), MainMenu() or PauseMenu() depending on the
+	// game state
 	ErrorType Main();
 
-   // Called each frame when in the pause state. Manages the pause menu
-   // which is currently a basic placeholder
-	ErrorType PauseMenu();
-
-   // Called each frame when in the menu state. Manages the main menu,
-   // which is currently a basic placeholder.
+	// Called each frame when in the menu state. Manages the main menu,
+	// which is currently a basic placeholder.
 	ErrorType MainMenu();
 
-   // Called at the start of the game - when changing state from MENU to RUNNING
-   // Use this to initialise the core game
+    // Called each frame when in the pause state. Manages the pause menu
+    // which is currently a basic placeholder
+	ErrorType PauseMenu();
+
+	// Called at the start of the game - when changing state from MENU to RUNNING
+	// Use this to initialise the core game
 	ErrorType StartOfGame();
 
-   // Called each frame when in the RUNNING state.
-   // Checks for user pressing escape (which puts the game in the PAUSED state)
-   // Flips and clears the back buffer
-   // Gameplay programmer will develop this to create an actual game
+	// Called each frame when in the RUNNING state.
+	// Checks for user pressing escape (which puts the game in the PAUSED state)
+	// Flips and clears the back buffer
+	// Gameplay programmer will develop this to create an actual game
 	ErrorType Update();
 
-   // Called when the player ends the game
-   // Currently this is done from the PAUSED state, when returning to the main menu
-   // but could be done by the gameplay programmer in other situations
-   // This will be used by the gameplay programmer to clean up
+	// Called when the player ends the game
+	// Currently this is done from the PAUSED state, when returning to the main menu
+	// but could be done by the gameplay programmer in other situations
+	// This will be used by the gameplay programmer to clean up
 	ErrorType EndOfGame();
 };
-
-
-
-
